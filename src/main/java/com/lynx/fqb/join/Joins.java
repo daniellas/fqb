64b84237ec74bdd285101188d788594c4727d957
Daniel Las <lasdaniel@gmail.com>
2018-02-06 04:54:39 +0100

    Removed unnecessary public modifiers

--- src/main/java/com/lynx/fqb/join/Joins.java
+++ src/main/java/com/lynx/fqb/join/Joins.java
@@ -16,13 +16,13 @@ public interface Joins {
 
     @SuppressWarnings("unchecked")
     @SafeVarargs
-    public static <A> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>[]> of(BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>>... joins) {
+    static <A> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>[]> of(BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>>... joins) {
         return (cb, from) -> {
             return Arrays.stream(joins).map(j -> j.apply(cb, from)).toArray(Join[]::new);
         };
     }
 
-    public static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> join(SingularAttribute<? super A, B> attr, JoinType type,
+    static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> join(SingularAttribute<? super A, B> attr, JoinType type,
             Optional<BiFunction<CriteriaBuilder, Path<? extends B>, Predicate[]>> predicates) {
         return (cb, from) -> {
             Join<A, B> join = from.join(attr, type);
@@ -35,21 +35,21 @@ public interface Joins {
         };
     }
 
-    public static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> inner(SingularAttribute<? super A, B> attr,
+    static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> inner(SingularAttribute<? super A, B> attr,
             BiFunction<CriteriaBuilder, Path<? extends B>, Predicate[]> predicates) {
         return join(attr, JoinType.INNER, Optional.of(predicates));
     }
 
-    public static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> inner(SingularAttribute<? super A, B> attr) {
+    static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> inner(SingularAttribute<? super A, B> attr) {
         return join(attr, JoinType.INNER, Optional.empty());
     }
 
-    public static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> left(SingularAttribute<? super A, B> attr,
+    static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> left(SingularAttribute<? super A, B> attr,
             BiFunction<CriteriaBuilder, Path<? extends B>, Predicate[]> predicates) {
         return join(attr, JoinType.LEFT, Optional.of(predicates));
     }
 
-    public static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> left(SingularAttribute<? super A, B> attr) {
+    static <A, B> BiFunction<CriteriaBuilder, From<A, A>, Join<A, ?>> left(SingularAttribute<? super A, B> attr) {
         return join(attr, JoinType.LEFT, Optional.empty());
     }
 
